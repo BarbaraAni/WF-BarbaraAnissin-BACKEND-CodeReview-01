a3246ef35a25303c7a7393cb768a0d6b246ec608
Barbara Anissin <anissinb@gmail.com>
2019-11-23 01:46:59 +0100

    bonus finished with do-while loop

--- src/CodeReview01.java
+++ src/CodeReview01.java
@@ -1,44 +1,49 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class CodeReview01 {
-    public static void calculateRectArea(int width, int height) {
-        Scanner scanner = new Scanner(System.in);
-        //System.out.println("Please input the rectangular width in cm:");
-        //width = scanner.nextInt();
-        //System.out.println("Please input the rectangular height in cm:");
-        //height = scanner.nextInt();
+    private static void calculateRectArea(int width, int height) {
         int area = height * width;
         System.out.println("The rectangular with a height of "+height+" and a width of "+width+" has an area of "+area+ "cm²");
-
-        int[][] recArr = new int[4][2];
-        for (int i=0; i<recArr.length; i++) {
-            recArr[i][0] = height;
-            recArr[i][1] = width;
-        }
-        System.out.println("recArr"+recArr[0][0]+" "+recArr[0][1]+" "+recArr[1][0]+" "+recArr[1][1]+" "+recArr[2][0]+" "+recArr[2][1]+" "+recArr[3][0]+" "+recArr[3][1]);
     }
-    public static void squareArea(int side){
-        int sqArr[] = new int [4];
-        for (int i=0; i<sqArr.length; i++){
-            sqArr[i] = side;
-        }
+    private static void squareArea(int side){
         int sqArea = side*side;
         int sqPerimeter = side*4;
         System.out.println("Square-Area with side-length "+side+" is: "+sqArea+"cm²");
         System.out.println("Square-Circumference with side-length "+side+" is: "+sqPerimeter+"cm²");
     }
+
+    private static void bonus() {
+        Scanner scanner = new Scanner(System.in);
+        ArrayList<Integer> width = new ArrayList<>();
+        ArrayList<Integer> height = new ArrayList<>();
+        int now = 0;
+        do {
+            System.out.println("==Enter width==");
+            int x = scanner.nextInt();
+            System.out.println("==Enter height==");
+            int y = scanner.nextInt();
+            width.add(x);
+            height.add(y);
+            int ar = (width.get(now))*(height.get(now));
+            System.out.println("==RESULT==\nArea = "+ar);
+            now++;
+        } while (now<4);
+    }
     public static void main(String[] args) {
+        System.out.println("====RECTANGLE====");
         calculateRectArea(20,30);
         calculateRectArea(40,40);
         calculateRectArea(10,60);
         calculateRectArea(5,2);
+        System.out.println("====SQUARE====");
         squareArea(2);
         squareArea(4);
         squareArea(6);
         squareArea(23);
-
-        // (10) Save your test data (several widths / heights) for several different rectangles in two arrays.
-        // Use the for loop to access the array elements, then perform rectangular surface calculation and display results
+        System.out.println("====BONUS Part 1====");
+        bonus();
+        System.out.println("====BONUS Part 2====");
 
         // (10) create a Java program (.java) that can accept width and height of a rectangular as parameters to main()
         // and displays a solution in console. To do that, do some research on java programs that accept parameters.
